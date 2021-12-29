649400399b106d246d6b3b5c15034613455d806e
Ubeydullah Yavuz <38444243+Hubbey@users.noreply.github.com>
2021-12-30 00:50:59 +0300

    Add files via upload

---
+++ serv.js
@@ -0,0 +1,22 @@
+const { RSA_PKCS1_OAEP_PADDING } = require('constants');
+var http = require('http'); // Import Node.js core module
+
+var server = http.createServer(function (req, res) {   //create web server
+        
+	var path = req.url
+	var port = path.substring(1)
+    	var loc = "http://127.0.0.1:" + port
+	var ip = "http://" + port + "/"
+        // set response header
+        res.writeHead(302, { 'Location': 'https://' }); 
+        
+        // set response content    
+        res.write(path);
+        res.end();
+    
+
+});
+
+server.listen(process.env.PORT); //6 - listen for any incoming requests
+
+console.log('Node.js web server is running..')
