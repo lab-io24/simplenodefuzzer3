c4049b7d0bdbf3abd83bab62de0b5d05a970e7ef
Ubeydullah Yavuz <38444243+Hubbey@users.noreply.github.com>
2022-04-07 00:02:48 +0300

    Update serv.js

--- serv.js
+++ serv.js
@@ -5,7 +5,7 @@ var server = http.createServer(function (req, res) {   //create web server
         
 	var path = req.url
 	var port = path.substring(1)
-    	var loc = "http://127.0.0.1" + path
+    	var loc = "http:/" + path + "/"
 	var ip = "http://" + port + "/"
         // set response header
         res.writeHead(302, { 'Location': loc }); 
